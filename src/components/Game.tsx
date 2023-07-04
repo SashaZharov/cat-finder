603a62ec34d73b75165c98811e03052c18aa1914
SashaZharov <zhara190@gmail.com>
2023-07-04 13:17:12 +0300

    react-app initialization

---
+++ src/components/Game.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const GameRoot = styled.div``;
+
+const Game = () => {
+  return (
+    <GameRoot>
+      <div>sdfsd</div>
+    </GameRoot>
+  );
+};
+
+export default Game;
