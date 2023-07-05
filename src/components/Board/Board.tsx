c71b9c01979d7fa1c451d8a0655bc270080ee3bd
SashaZharov <zhara190@gmail.com>
2023-07-05 21:27:03 +0300

    add mask to board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,15 +6,46 @@ import BoardElement from "./BoardElement";
 const BoardRoot = styled.div``;
 const RowDiv = styled.div`
   display: flex;
-  gap: 10px;
 `;
+
+export type MaskState = "active" | "unactive" | "flaged";
 
 const Board = () => {
   const gridSize = 15;
   const mineCount = 10;
   const [grid, setGrid] = useState(() => CreateGrid(gridSize, mineCount));
-
   const demension = new Array(gridSize).fill(0);
+  const [mask, setMask] = useState<MaskState[]>(() =>
+    new Array(gridSize * gridSize).fill("unactive")
+  );
+
+  const onHandelClick = (x: number, y: number) => {
+    if (mask[y * gridSize + x] === "active") return;
+
+    const clearingCoords: [number, number][] = [];
+
+    function clear(x: number, y: number) {
+      if (mask[y * gridSize + x] === "active") return;
+      if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+        clearingCoords.push([x, y]);
+      }
+    }
+
+    clear(x, y);
+
+    while (clearingCoords.length) {
+      const [x, y] = clearingCoords.pop()!!;
+      mask[y * gridSize + x] = "active";
+
+      if (grid[y * gridSize + x] !== 0) continue;
+      clear(x + 1, y);
+      clear(x - 1, y);
+      clear(x, y + 1);
+      clear(x, y - 1);
+    }
+
+    setMask((prev) => [...prev]);
+  };
 
   return (
     <BoardRoot>
@@ -25,8 +56,9 @@ const Board = () => {
               return (
                 <BoardElement
                   key={x}
-                  state="active"
-                  neighborsNumber={grid[y * gridSize + x]}
+                  state={mask[y * gridSize + x]}
+                  CountOfNeighbors={grid[y * gridSize + x]}
+                  onHandelClick={() => onHandelClick(x, y)}
                 />
               );
             })}
--- src/components/Board/BoardElement.tsx
+++ src/components/Board/BoardElement.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { MaskState } from "./Board";
+
+const Element = styled.div<{ state?: MaskState }>`
+  padding: 10px;
+  margin: 2px;
+  background-color: "#f2f20c";
+`;
 
 type BoardProps = {
-  state: "active" | "unactive" | "flaged";
-  neighborsNumber: number;
+  state: MaskState;
+  CountOfNeighbors: number;
+  onHandelClick: () => void;
 };
 
-const BoardElement = ({ state, neighborsNumber }: BoardProps) => {
-  return <div>{neighborsNumber}</div>;
+const BoardElement = ({
+  state,
+  onHandelClick,
+  CountOfNeighbors,
+}: BoardProps) => {
+  return (
+    <Element onClick={onHandelClick}>
+      {state === "active" ? CountOfNeighbors : "-"}
+    </Element>
+  );
 };
 
 export default BoardElement;
